8339a2379c622d50261c43d4088e53252fd0cea4
TOmjayzhj <942934710@qq.com>
2024-10-24 17:49:43 +0800

    Add files via upload

---
+++ 输出平均分最高的学生.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(void)
+{
+	int i,n;
+	struct student{
+		int num;
+		char name[10];
+		int math,English,computer;
+		double average;
+	};
+	struct student stu,max;
+	//printf("Enter n:");
+	scanf("%d",&n);
+	//printf("Enter student num,name,course-score:\n");
+	//printf("No 1:");
+   // scanf("%d %s %i %i %i",&max.num,max.name,&max.math,&max.English,&max.computer);
+	//max.average=(max.math+max.English+max.computer)/3.0;
+	
+	for(i=1;i<=n;i++){
+		//printf("No %d:",i);
+		scanf("%d %s %i %i %i",&stu.num,stu.name,&stu.math,&stu.English,&stu.computer);
+		stu.average=(stu.math+stu.English+stu.computer)/3.0;
+		if(i==1){
+			max=stu;
+		}else if(stu.average>max.average){
+			max=stu;
+		}
+	}
+	
+	printf("num:%d,name:%s,average:%.2f",max.num,max.name,max.average);
+	return 0;
+}
