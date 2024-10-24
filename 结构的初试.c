2012e5bf3c910b32761a06bc42c849369f4c615a
TOmjayzhj <942934710@qq.com>
2024-10-24 13:03:32 +0800

    Update 结构的初试.c

--- 结构的初试.c
+++ 结构的初试.c
@@ -6,12 +6,12 @@ int main(void)
 		int day;
 		int year;
 	};
-	struct date today={12,25,2020};
+	struct date today={12,25,2020};                 //结构的两种使用
 	struct date  da;
 	da=(struct date){12,5,2019};
 	printf("da : %i %i %i\n",da.year,da.day,da.month);
 	today=da;
-	//today.day=24;
+	//today.day=24;                                  //结构的基本用法   
 	//today.month=10;
 	//today.year=2024;
 	printf("today is %i %i %i\n",today.year,today.month,today.day);
